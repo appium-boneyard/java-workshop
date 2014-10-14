f21b5fe546f4d6e00e61a5318ed47b22b5a9e543
Jonathan Lipps <jlipps@gmail.com>
2014-10-14 14:30:25 -0700

    finish writing first mobile test

--- src/test/java/io/appium/test/workshop/MobileWebTest.java
+++ src/test/java/io/appium/test/workshop/MobileWebTest.java
@@ -1,4 +1,55 @@
 package io.appium.test.workshop;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
 public class MobileWebTest {
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "iPhone Simulator");
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("platformVersion", "7.1");
+        capabilities.setCapability("browserName", "safari");
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),
+                capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void basicTest() throws Exception {
+        driver.get("http://saucelabs.com/test/guinea-pig");
+        WebElement idElement = driver.findElement(By.id("i_am_an_id"));
+        assertNotNull(idElement);
+        assertEquals("I am a div", idElement.getText());
+        WebElement commentElement = driver.findElement(By.id("comments"));
+        assertNotNull(commentElement);
+        commentElement.sendKeys("This is an awesome comment");
+        WebElement submitElement = driver.findElement(By.id("submit"));
+        assertNotNull(submitElement);
+        submitElement.click();
+        WebElement yourCommentsElement = driver.findElement(By.id("your_comments"));
+        assertNotNull(yourCommentsElement);
+        assertTrue(driver.findElement(By.id("your_comments")).getText().contains("This is an awesome comment"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 }
