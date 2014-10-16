c673ebb74f25fbb57fe83aa39a3ce92078ae9aa5
Jonathan Lipps <jlipps@gmail.com>
2014-10-16 12:34:16 -0700

    some more refactoring

--- src/test/java/io/appium/test/workshop/IOSMobileWebTest.java
+++ src/test/java/io/appium/test/workshop/IOSMobileWebTest.java
@@ -1,30 +1,21 @@
 package io.appium.test.workshop;
 
-import io.appium.java_client.ios.IOSDriver;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.*;
 
-public class MobileWebTest extends SauceTestClass {
+public class IOSMobileWebTest extends SauceTestClass {
 
     @Before
     public void setUp() throws Exception {
         capabilities.setCapability("deviceName", "iPhone Simulator");
-        capabilities.setCapability("platformName", "iOS");
         capabilities.setCapability("platformVersion", "7.1");
         capabilities.setCapability("browserName", "safari");
-        capabilities.setCapability("name", testName.getMethodName());
-        driver = new IOSDriver(new URL(sauceServer), capabilities);
-        sessionId = driver.getSessionId().toString();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        super.setUpIosDriver();
     }
 
     @Test
--- src/test/java/io/appium/test/workshop/SauceTestClass.java
+++ src/test/java/io/appium/test/workshop/SauceTestClass.java
@@ -4,12 +4,17 @@ import com.saucelabs.common.SauceOnDemandAuthentication;
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
 import com.saucelabs.junit.SauceOnDemandTestWatcher;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.URL;
 import java.text.MessageFormat;
+import java.util.concurrent.TimeUnit;
 
 public abstract class SauceTestClass  implements SauceOnDemandSessionIdProvider {
     protected AppiumDriver driver;
@@ -26,6 +31,27 @@ public abstract class SauceTestClass  implements SauceOnDemandSessionIdProvider 
 
     @Rule
     public SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    protected void setUpIosDriver() throws Exception {
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("name", testName.getMethodName());
+        capabilities.setCapability("appiumVersion", "1.3.0-beta1");
+        driver = new IOSDriver(new URL(sauceServer), capabilities);
+        setUpDriver();
+    }
+
+    protected void setUpAndroidDriver() throws Exception {
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("name", testName.getMethodName());
+        capabilities.setCapability("appiumVersion", "1.3.0-beta1");
+        driver = new AndroidDriver(new URL(sauceServer), capabilities);
+        setUpDriver();
+    }
+
+    protected void setUpDriver() throws Exception {
+        sessionId = driver.getSessionId().toString();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
 
     @After
     public void tearDown() throws Exception {
